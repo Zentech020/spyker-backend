073726414c34ebfcc31e68604541a6b5e4f4831f
Ysturkenboom <y.a.sturkenboom@gmail.com>
2018-12-22 17:03:50 +0100

    Nieuwe mailclient

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var base = new Airtable({ apiKey: 'keyer1jczZsNjdA4C' }).base(
   'apperUKb615OJ5vIW'
 );
 var _ = require('lodash');
-const sgMail = require('@sendgrid/mail');
+const mailjet = require('node-mailjet')
+ .connect("75ca55cb6caac913e69197ee2ded0739", "ebcbaa652b9712fb8068f64432c5d36d");
 
 const start = () => {
   let allDeals = [];
@@ -38,7 +39,6 @@ const start = () => {
 }
 
 const checkForAllDeals = (users, deals) => {
-  console.log(users.length, deals.length);
   users.map(user => checkForDeals(user, deals))
 }
 
@@ -49,18 +49,18 @@ const checkForDeals = (record, deals) => {
 
   deals.map(deal => {
     if (record.get('P1-complete') === 0) {
-      compareDeal(base('User data'), record, deal.get('Name'), 'P1');
+      compareDeal(base('User data'), record, deal.get('Name'), 'P1', deal);
     }
     if (record.get('P2-complete') === 0) {
-      compareDeal(base('User data'), record, deal.get('Name'), 'P2');
+      compareDeal(base('User data'), record, deal.get('Name'), 'P2', deal);
     }
     if (record.get('P3-complete') === 0) {
-      compareDeal(base('User data'), record, deal.get('Name'), 'P3');
+      compareDeal(base('User data'), record, deal.get('Name'), 'P3', deal);
     }
   });
 };
 
-const compareDeal = (base, record, dealTitle, customerProductNo) => {
+const compareDeal = (base, record, dealTitle, customerProductNo, dealObj) => {
   if (
     dealTitle
       .toLowerCase()
@@ -69,25 +69,50 @@ const compareDeal = (base, record, dealTitle, customerProductNo) => {
     const updateObj = {};
     updateObj[customerProductNo + '-complete'] = 1;
     base.update(record.id, updateObj);
-    sendMail(record.get('Email'), record.get('Name'), dealTitle);
+    sendMail(record.get('Email'), record.get('First name'), dealTitle, dealObj);
   }
 };
 
-const sendMail = (email, name, productTitle) => {
-  console.log('Mailtje', email, productTitle);
-  sgMail.setApiKey(sendGridAPI);
+const sendMail = (email, name, productTitle, deal) => {
+  const request = mailjet
+   .post("send", { 'version': 'v3.1' })
+   .request({
+    "Messages": [
+     {
+      "From": {
+       "Email": "zenno.bruinsma@gmail.com",
+       "Name": "Spyker team"
+      },
+      "To": [
+       {
+        "Email": email,
+        "Name": name
+       }
+      ],
+      "TemplateID": 626166,
+      "TemplateLanguage": true,
+      "Subject": "Spyker - "+ productTitle +" aanbieding!",
+      "Variables": {
+       "product": productTitle,
+       "name": name,
+       "store": "Albert Heijn",
+       "description": deal.get('description'),
+       "deal": deal.get('Name'),
+       "price_to":  deal.get('priceTo'),
+       "price_from": deal.get('priceFrom'),
+       "valid_to": deal.get('validUntil')
+      }
+     }
+    ]
+   })
 
-  const msg = {
-    to: 'zenno@storyofams.com',
-    from: 'sender@example.org',
-    templateId: 'd-ffe6708f1cf14057ad8b28bd06882140',
-    dynamic_template_data: {
-      subject: 'Testing Templates',
-      name: name,
-      product: productTitle
-    }
-  };
-  sgMail.send(msg);
+  request
+   .then((result) => {
+    console.log(result.body)
+   })
+   .catch((err) => {
+    console.log(err)
+   })
 };
 
 // cron.schedule('* * * * *', () => {
