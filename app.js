03a3800afeae926ba2ffbe527569be66c23ff9e0
Zenno Bruinsma <zenno.bruinsma@bloomon.nl>
2018-12-23 11:22:42 +0100

    push

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var base = new Airtable({ apiKey: 'keyer1jczZsNjdA4C' }).base(
 );
 var _ = require('lodash');
 const mailjet = require('node-mailjet')
- .connect("75ca55cb6caac913e69197ee2ded0739", "ebcbaa652b9712fb8068f64432c5d36d");
+  .connect("75ca55cb6caac913e69197ee2ded0739", "ebcbaa652b9712fb8068f64432c5d36d");
 
 const start = () => {
   let allDeals = [];
@@ -75,48 +75,48 @@ const compareDeal = (base, record, dealTitle, customerProductNo, dealObj) => {
 
 const sendMail = (email, name, productTitle, deal) => {
   const request = mailjet
-   .post("send", { 'version': 'v3.1' })
-   .request({
-    "Messages": [
-     {
-      "From": {
-       "Email": "zenno.bruinsma@gmail.com",
-       "Name": "Spyker team"
-      },
-      "To": [
-       {
-        "Email": email,
-        "Name": name
-       }
-      ],
-      "TemplateID": 626166,
-      "TemplateLanguage": true,
-      "Subject": "Spyker - "+ productTitle +" aanbieding!",
-      "Variables": {
-       "product": productTitle,
-       "name": name,
-       "store": "Albert Heijn",
-       "description": deal.get('description'),
-       "deal": deal.get('Name'),
-       "price_to":  deal.get('priceTo'),
-       "price_from": deal.get('priceFrom'),
-       "valid_to": deal.get('validUntil')
-      }
-     }
-    ]
-   })
+    .post("send", { 'version': 'v3.1' })
+    .request({
+      "Messages": [
+        {
+          "From": {
+            "Email": "zenno.bruinsma@gmail.com",
+            "Name": "Spyker team"
+          },
+          "To": [
+            {
+              "Email": email,
+              "Name": name
+            }
+          ],
+          "TemplateID": 626166,
+          "TemplateLanguage": true,
+          "Subject": "Spyker - " + productTitle + " aanbieding!",
+          "Variables": {
+            "product": productTitle,
+            "name": name,
+            "store": deal.get('supermarket'),
+            "description": deal.get('description'),
+            "deal": deal.get('Name'),
+            "price_to": deal.get('priceTo'),
+            "price_from": deal.get('priceFrom'),
+            "valid_to": deal.get('validUntil')
+          }
+        }
+      ]
+    })
 
   request
-   .then((result) => {
-    console.log(result.body)
-   })
-   .catch((err) => {
-    console.log(err)
-   })
+    .then((result) => {
+      console.log(result.body)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 };
 
-// cron.schedule('* * * * *', () => {
-//   start();
-// });
+cron.schedule('* * * * *', () => {
+  start();
+});
 
 start();
